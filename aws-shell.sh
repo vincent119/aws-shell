fd6dfedba7c4bbb2bb461d1a22e7026af7e1abb2
vincent <vincent@twroa.com>
2023-07-13 14:20:59 +0800

    1

--- aws-shell.sh
+++ aws-shell.sh
@@ -6,56 +6,14 @@ ec2_list() {
 
 aws_region_all_name(){
   fmt="%-16s%-4s%-25s\n"
-  for i in $(aws ec2 describe-regions --query 'Regions[].RegionName' --output text)
+  for i in $(aws ec2 describe-regions  --all-regions  --query 'Regions[].RegionName' --output text)
   do 
     printf "$fmt" "'$i" "=>" "$(aws ssm get-parameter --name /aws/service/global-infrastructure/regions/$i/longName --query "Parameter.Value" --output text)',"
   done
-
 }
 
 aws_region_All(){
-aws ec2 describe-regions \
-    --all-regions \
-    --query "Regions[].{Name:RegionName}" \
-    --output table
-}
-
-aws_region_set(){
-  ENV=$1
-  if [[ -z $ENV ]];then
-    echo "environment list...."
-    echo "uat,dev,prod,im"
-  fi
-  if [[ $ENV == "uat" ]];then
-    export AWS_REGION="ap-northeast-1"
-    export AWS_PROFILE="uat-profile"
-    echo "environment is UAT"
-    echo "AWS Region is Tokyo"
-    echo "AWS_REGION=ap-northeast-1"
-  elif [[ $ENV == "dev" ]];then
-    export AWS_REGION="ap-east-1"
-    export AWS_PROFILE="default"
-    echo "environment is Dev"
-    echo "AWS Region is Hong Kong"
-    echo "AWS_REGION=ap-east-1"
-  elif [[ $ENV == "prod" ]];then
-    export AWS_REGION="ap-southeast-1"
-    export AWS_PROFILE="prod-profile"
-    echo "environment is Master"
-    echo "AWS Region is Sigapore"
-    echo "AWS_REGION=ap-southeast-1"
-  elif [[ $ENV == "im" ]];then
-    export AWS_REGION="ap-east-1"
-    export AWS_PROFILE="im-profile"
-    echo "environment is IM-devops"
-    echo "AWS Region is Hong Kong"
-    echo "AWS_REGION=ap-east-1"
-  fi
-
-
-}
-aws_ec2_instance_list(){
-  aws ec2 describe-instance-types --no-paginate  --filters Name=current-generation,Values=true --query "InstanceTypes[*].{InstanceType_name: InstanceType,vcpus: VCpuInfo.DefaultVCpus, memory_in_mib: MemoryInfo.SizeInMiB}" --output table
+  aws ec2 describe-regions --all-regions --query "Regions[].{Name:RegionName}" --output table
 }
 aws_az_list() {
   aws ec2 describe-availability-zones --region $AWS_REGION --query "AvailabilityZones[*].{RegionName:RegionName,ZoneName:ZoneName,ZoneId:ZoneId}" --output table
@@ -69,11 +27,12 @@ aws_elb_Get() {
    "LoadBalancers[].[LoadBalancerName,AvailabilityZones[0].ZoneName,AvailabilityZones[0].SubnetId,AvailabilityZones[1].ZoneName,AvailabilityZones[1].SubnetId]" --output table
 
 }
+
 aws_elb_Tag() {
     aws elbv2 describe-target-groups --query \
-    "TargetGroups[].[TargetGroupArn,TargetGroupName,Protocol,Port,HealthCheckProtocol,HealthCheckPort,HealthCheckEnabled,HealthCheckIntervalSeconds,HealthCheckTimeoutSeconds,HealthyThresholdCount,UnhealthyThresholdCount,HealthCheckPath,TargetType,Matcher.HttpCode]"  \
-    --output table     
+    "TargetGroups[].[TargetGroupArn,TargetGroupName,Protocol,Port,HealthCheckProtocol,HealthCheckPort,HealthCheckEnabled,HealthCheckIntervalSeconds,HealthCheckTimeoutSeconds,HealthyThresholdCount,UnhealthyThresholdCount,HealthCheckPath,TargetType,Matcher.HttpCode]" --output table
 }
+
 aws_fw_Rule() {
 aws ec2 describe-security-groups --filters Name=ip-permission.cidr,Values='0.0.0.0/0' --query "SecurityGroups[*].{Name:GroupName,vpc:VpcId,sg:GroupId }" --output table
 }
@@ -92,3 +51,37 @@ aws route53 list-resource-record-sets   --hosted-zone-id $rid --query 'ResourceR
 aws_keypair_list() {
    aws ec2  describe-key-pairs --output table
 }
+aws_waf_list() {
+  aws  wafv2  list-web-acls --scope  REGIONAL  --query 'WebACLs[*].{Id:Id,Name:Name,ARN:ARN}' --output table
+}
+
+aws_cf_distributions() {
+  aws cloudfront list-distributions --output table --query 'DistributionList.Items[*].[Id,Origins.Items[0].DomainName]'
+}
+
+# aws_cf_distributions_conf() {
+#   rid=$1
+#   aws cloudfront get-distribution --id $rid
+# }
+
+awsACMlist(){
+  aws acm list-certificates --query "CertificateSummaryList[*].{ARN:CertificateArn,DomainName:DomainName,Status:Status}"  --output table
+}
+awsEcrList(){
+  aws ecr describe-repositories --query "repositories[*].{repositoryName:repositoryName,repositoryUri:repositoryUri}" --output table
+}
+aws_ec2_instance_list(){
+  aws ec2 describe-instance-types --no-paginate  --filters Name=current-generation,Values=true --query "InstanceTypes[*].{InstanceType_name: InstanceType,vcpus: VCpuInfo.DefaultVCpus, memory_in_mib: MemoryInfo.SizeInMiB}" --output table
+}
+awsEc2List(){
+  aws ec2 describe-instances --query "Reservations[].Instances[].{Name:Tags[?Key==\`Name\`].Value|[0],InstanceId:InstanceId,ImageId:ImageId,InstanceType:InstanceType,KeyName:KeyName,PrivateIpAddress:PrivateIpAddress,PublicIpAddress:PublicIpAddress,State:State.Name}" --output table
+}
+awsEc2IamList(){
+  aws ec2 describe-iam-instance-profile-associations --query "IamInstanceProfileAssociations[].{InstanceId:InstanceId,IamInstanceProfileARN:IamInstanceProfile.Arn,IamInstanceProfileID:IamInstanceProfile.Id}" --output table
+}
+
+LibPATH="/Users/vincent/Documents/git_home/vin/aws-shell"
+
+. $LibPATH/aws-region-change.sh
+. $LibPATH/crypto.sh
+. $LibPATH/pulumi.sh
